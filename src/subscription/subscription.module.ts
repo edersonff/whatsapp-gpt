c5df5ed02ec60a0bcd8b898292202561558ed5e2
ederson <ederson@bemobile.tech>
2024-05-07 04:21:53 -0300

    fix: subscription controller added in module

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './entities/subscription.entity';
+import { SubscriptionController } from './subscription.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscription])],
   exports: [TypeOrmModule, SubscriptionService],
+  controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
 export class SubscriptionModule {}
